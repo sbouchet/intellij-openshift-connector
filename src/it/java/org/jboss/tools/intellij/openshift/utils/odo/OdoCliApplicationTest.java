010b82f719d5090b4f9d425ea5b37e6125203656
Stephane Bouchet <sbouchet@redhat.com>
2021-02-11 12:19:34 +0100

    Update OdoCliApplicationTest.java

--- src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliApplicationTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliApplicationTest.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(Parameterized.class)
 public class OdoCliApplicationTest extends OdoCliTest {
